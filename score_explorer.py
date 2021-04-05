3d5a2c81769f253465500eed52c8a7f3b7c387cf
Eric Anderson <eric@booksuey.com>
2021-04-05 15:04:25 -0400

    Added page config

--- score_explorer.py
+++ score_explorer.py
@@ -5,6 +5,12 @@ import numpy as np
 import pandas as pd
 import altair as alt
 
+st.set_page_config(  # Alternate names: setup_page, page, layout
+	layout="wide",  # Can be "centered" or "wide". In the future also "dashboard", etc.
+	initial_sidebar_state="expanded",  # Can be "auto", "expanded", "collapsed"
+	page_title='CogAT Score Explorer',  # String or None. Strings get appended with "• Streamlit". 
+	page_icon=None,  # String, anything supported by st.image, or None.
+)
 
 ############ Data Import
 cogat = pd.read_csv('trimmed.csv')
